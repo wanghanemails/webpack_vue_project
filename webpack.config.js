b1c6283f3855638eaee8044cf7df875e1ac3e595
carrywanghan <carrywanghan@didichuxing.com>
2018-10-15 16:24:15 +0800

    webpack new

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+
+module.exports = {
+    entry: "./src/main.js",
+    output: {
+        filename: "[name]-[chunkhash].js",
+        path: path.resolve(__dirname, "dist")
+
+    }
+
+
+
+
+}
