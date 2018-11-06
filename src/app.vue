ee0963707323b474d3ad89b30e13f7650f07377d
carrywanghan <carrywanghan@didichuxing.com>
2018-11-06 11:37:30 +0800

    webpack1

---
+++ src/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{test}}</div>
+</template>
+<script>
+import Vue from 'vue'
+export default {
+  data(){
+    return {
+      name = 'as'
+    }
+}
+}
+</script>
+<style lang="less">
+
+</style>
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,58 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+// const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-    entry: "./src/main.js",
-    output: {
-        filename: "[name]-[chunkhash].js",
-        path: path.resolve(__dirname, "dist/js")
+  entry: './src/main.js',
+  output: {
+    filename: '[name]-[chunkhash].js',
+    path: path.resolve(__dirname, 'dist/js')
 
-    },
-    module: {
-        rules: [{
-            test: "/\.css$/",
-            use: ExtractTextPlugin.extract({
-                fallback: "style-loader",
-                use: {
-                    loader: "css-loader",
-                    options: {
-                        minimize: true //css压缩
-                    }
-                }
-
-            })
-
-        }]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: __dirname + "/app/index.tmpl.html" //new 一个这个插件的实例，并传入相关的参数
-        }),
-        new ExtractTextPlugin({
-            filename: './dist/css/[name]-[contenthash].css',
-        }),
+  },
+  module: {
+    rules: [{
+      test:  /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader'
+      ]},  
+    {
+      test:/\.less$/,
+      use:[
+        {
+          'loader':'style-loader'
+        } ,{
+          'loader':'css-loader',
+          options: {
+            sourceMap: true
+          }
+        },{
+          'loader':'less-loader',
+          options: {
+            sourceMap: true
+          }
+        }] 
+    } ,
+    {
+      test:/\.(png|svg|jpg|gif)$/,
+      use:[
+        'file-loader' 
+      ]} 
     ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+    // new HtmlWebpackPlugin({
+    //   template: __dirname + '/app/index.tmpl.html' //new 一个这个插件的实例，并传入相关的参数
+    // }),
+    // new ExtractTextPlugin({
+    //   filename: './dist/css/[name]-[contenthash].css',
+    // }),
+  ]
 
 
 
